0212ff762bd2889aec78149dd109ecf47d82361b
Alexander Tsygankov <acygankov616@gmail.com>
2025-12-08 01:06:06 +0300

    fix: add polyfills for Node 20+ timers in setupTests.js to ensure compatibility with @testing-library helpers

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// Polyfill timers for Node 20+ so @testing-library helpers work.
+global.setImmediate =
+    global.setImmediate || ((fn, ...args) => setTimeout(fn, 0, ...args));
+global.clearImmediate =
+    global.clearImmediate || ((id) => clearTimeout(id));
+
 import '@testing-library/jest-dom';
